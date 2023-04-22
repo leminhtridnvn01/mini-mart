2f65a5fb350ad02393bfdd213453e1dc7f1c3151
tri_le <tri_le@datahouse.com>
2023-04-22 15:30:04 +0700

    Create list Category

--- src/app/product-category/components/product-category-queue-grid/product-category-queue-grid.component.ts
+++ src/app/product-category/components/product-category-queue-grid/product-category-queue-grid.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { GetAllCategoriesRequest } from '../../models/GetAllCategoriesRequest';
+import { GetAllCategoriesResponse } from '../../models/GetAllCategoriesResponse';
+import { ProductCategoryService } from './../../services/product-category.service';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-category-queue-grid',
   templateUrl: './product-category-queue-grid.component.html',
-  styleUrls: ['./product-category-queue-grid.component.css']
+  styleUrls: ['./product-category-queue-grid.component.css'],
 })
-export class ProductCategoryQueueGridComponent {
+export class ProductCategoryQueueGridComponent implements OnInit {
+  categories: GetAllCategoriesResponse[];
+  constructor(private productCategoryService: ProductCategoryService) {}
+  ngOnInit(): void {
+    this.InitData();
+  }
 
+  InitData(): void {
+    var request: GetAllCategoriesRequest = {
+      skip: 0,
+      take: 5,
+    };
+    this.productCategoryService
+      .getAllCategories(request)
+      .subscribe((result) => {
+        if (result) {
+          this.categories = result.data;
+        }
+      });
+  }
 }
